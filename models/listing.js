9412f811ab2e8c615d6fc8e3928da67dbfadd46e
Rishabh Shankar Prasad <rishabhsp125@gmail.com>
2026-06-03 17:56:26 +0530

    Initialize random fake data, Listing Model, Implement CURD routes

---
+++ models/listing.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1561501900-3701fa6a0864?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1561501900-3701fa6a0864?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        : v,
+  },
+  price: {
+    type: Number,
+    require: true,
+  },
+  country: {
+    type: String,
+    require: true,
+  },
+  location: {
+    type: String,
+    require: true,
+  },
+});
+
+const Listings = mongoose.model("Listings", listingSchema);
+
+module.exports = Listings;
